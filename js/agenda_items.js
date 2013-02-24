ea8d5986528bf82cb9dcccda289ef45d6b08ed04
Kat Chuang <katychuang@gmail.com>
2013-02-24 14:45:56 -0500

    updated challenge and program pages

---
+++ js/agenda_items.js
@@ -0,0 +1,190 @@
+// Copyright 2011 Google Inc.  All Rights Reserved.
+
+/**
+ * @fileoverview Google IO agenda data stored in objects for dynamic
+ * interaction in the site UI.
+ */
+
+/**
+ * Holds agenda items that populate interactive agenda page.
+ * @type {Object.<string>}
+ */
+var agendaData = [
+  {
+
+    'date': 'Tuesday, April 2nd, 2013',
+    'schedule': [
+
+      {'start': '7:30',
+       'end': '14:00',
+       'title': 'Registration',
+       'location': 'Lobby'},
+
+      {'start': '9:00',
+       'end': '12:00',
+       'title': 'Morning Tutorials',
+       'location': 'Room 1'},
+
+       {'start': '12:00',
+       'end': '13:30',
+       'title': 'Lunch on your own',
+       'location': ''},
+
+       {'start': '13:30',
+       'end': '16:30',
+       'title': 'Afternoon Tutorials',
+       'location': 'Room 1'}
+
+
+    ]
+  },
+
+  {
+
+    'date': 'Wednesday, April 3rd, 2013',
+    'schedule': [
+
+      {'start': '8:00',
+       'end': '16:00',
+       'title': 'Registration',
+       'location': 'Lobby'},
+
+      {'start': '8:15',
+       'end': '8:30',
+       'title': 'Welcome & Admin',
+       'location': ''},
+
+      {'start': '8:30',
+       'end': '9:00',
+       'title': 'Keynote by Myron Gutmann, NSF',
+       'location': ''},
+
+      {'start': '9:15',
+       'end': '11:00',
+       'title': 'Morning Session',
+       'location': ''},
+
+      {'start': '11:00',
+       'end': '12:00',
+       'title': 'Challenge session',
+       'location': ''}, 
+
+      {'start': '12:00',
+       'end': '13:00',
+       'title': 'Lunch served *',
+       'location': ''},
+
+      {'start': '13:00',
+       'end': '15:00',
+       'title': 'Afternoon Session',
+       'location': ''},
+
+       {'start': '15:00',
+       'end': '17:00',
+       'title': 'Cross-Fertilization Roundtables',
+       'location': ''}
+
+
+    ]
+  },
+
+  {
+
+    'date': 'Thursday, April 4th, 2013',
+    'schedule': [
+
+      {'start': '8:00',
+       'end': '13:00',
+       'title': 'Registration',
+       'location': 'Lobby'},
+
+      {'start': '8:15',
+       'end': '8:30',
+       'title': 'Welcome & Admin',
+       'location': ''},
+
+      {'start': '8:30',
+       'end': '9:00',
+       'title': 'Keynote by Berndardo Huberman, HP',
+       'location': ''},
+
+      {'start': '9:15',
+       'end': '11:00',
+       'title': 'Morning Session',
+       'location': ''},
+
+      {'start': '11:00',
+       'end': '12:00',
+       'title': 'Challenge session',
+       'location': ''}, 
+
+      {'start': '12:00',
+       'end': '13:00',
+       'title': 'Lunch served *',
+       'location': ''},
+
+      {'start': '13:00',
+       'end': '16:00',
+       'title': 'Poster Intros',
+       'location': ''},
+
+       {'start': '16:00',
+       'end': '18:30',
+       'title': 'Poster Reception',
+       'location': ''}
+
+    ]
+  },
+  {
+
+    'date': 'Friday, April 5th, 2013',
+    'schedule': [
+
+      {'start': '8:00',
+       'end': '13:00',
+       'title': 'Registration',
+       'location': 'Lobby'},
+
+      {'start': '8:00',
+       'end': '8:15',
+       'title': 'Welcome & Admin',
+       'location': ''},
+
+      {'start': '8:15',
+       'end': '9:00',
+       'title': 'Keynote by Michele Gelfand, UMCP',
+       'location': ''},
+
+      {'start': '9:00',
+       'end': '11:45',
+       'title': 'Morning Session',
+       'location': ''},
+
+      {'start': '11:45',
+       'end': '13:15',
+       'title': 'Lunch served *',
+       'location': ''}, 
+
+      {'start': '11:45',
+       'end': '13:00',
+       'title': 'Funding panel',
+       'location': ''},
+
+      {'start': '13:15',
+       'end': '15:00',
+       'title': 'Afternoon Session 1',
+       'location': ''},
+
+      {'start': '15:00',
+       'end': '16:00',
+       'title': 'Afternoon Session 2',
+       'location': ''},
+
+       {'start': '16:00',
+       'end': '17:00',
+       'title': 'Conference Wrap-Up',
+       'location': ''}
+
+    ]
+  }
+];
